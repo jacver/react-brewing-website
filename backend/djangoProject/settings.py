334e986d03d82e06e519f55b9b8e98eac855e897
Jacob Vernau <jacobvernau@gmail.com>
2022-07-04 18:59:48 -0600

    hiding secret key

--- backend/djangoProject/settings.py
+++ backend/djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config # for env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xi0kbcmh%2d8gk*fv3-^_s2havsice+i)^@#=^!!e6135zx$tj'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', # added for DRF
     'corsheaders', # added for cors
-    'djangoApp' # API folder
+    'djangoApp' # App/API folder
 ]
 
 MIDDLEWARE = [
