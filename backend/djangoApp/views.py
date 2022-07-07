eb35a9bb2a9ac0fba5065e5dde1e41c27a19e002
Jacob Vernau <jacobvernau@gmail.com>
2022-07-06 18:46:10 -0600

    added requests for food, event, and merch models

--- backend/djangoApp/views.py
+++ backend/djangoApp/views.py
@@ -8,25 +8,48 @@ from rest_framework import status
 from .models import Beer, Food, Event, Merchandise
 from .serializers import *
 
+
+
+## Beer ##
+class BeerList(generics.ListCreateAPIView):
+    queryset = Beer.objects.all()
+    serializer_class = BeerSerializer
+
 def list_beer(request):
     beer = Beer.objects.all().values()
     beer_list = list(beer)
     return JsonResponse(beer_list, safe=False)
 
-class BeerList(generics.ListCreateAPIView):
-    queryset = Beer.objects.all()
-    serializer_class = BeerSerializer
 
+## Food ##
 class FoodList(generics.ListCreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 
+def food_list(request):
+    food = Food.objects.all().values()
+    food_list = list(food)
+    return JsonResponse(food_list, safe=False)
+
+## Event ##
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+def event_list(request):
+    event = Event.objects.all().values()
+    event_list = list(event)
+    return JsonResponse(event_list, safe=False)
+
+
+## Merchandise ## 
 class MerchandiseList(generics.ListCreateAPIView):
     queryset = Merchandise.objects.all()
     serializer_class = MerchandiseSerializer
+
+def merchandise_list(request):
+    merchandise = Merchandise.objects.all().values()
+    merchandise_list = list(merchandise)
+    return JsonResponse(merchandise_list, safe=False)
 
 
