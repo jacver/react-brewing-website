595911a43cdbcb2a5f17a294c28d2b09f39841e2
Jacob Vernau <jacobvernau@gmail.com>
2022-07-10 13:36:49 -0600

    added scroll arrows with bounce animation to hero. No links yet

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,14 +3,22 @@
 import React from "react";
 import "./hero.css";
 
+// React Icon Import
+import { HiChevronDoubleDown } from "react-icons/hi";
+
 function Hero() {
   return (
-    <section className="hero">
-      <div className="hero-inner">
-        <h1>SLICK DADDY's BREWING CO</h1>
-        <h2>beer beer beer beer beer</h2>
+    <div className="hero d-flex flex-column justify-content-evenly">
+      <section>
+        <div className="hero-inner">
+          <h1>SLICK DADDY's BREWING CO</h1>
+          <h2>beer beer beer beer beer</h2>
+        </div>
+      </section>
+      <div className="hero-scroll" style={{ fontSize: "5rem" }}>
+        <HiChevronDoubleDown />
       </div>
-    </section>
+    </div>
   );
 }
 
