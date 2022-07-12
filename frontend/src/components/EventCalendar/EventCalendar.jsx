2c2d948811fc20c998f302783dd82df46a331130
Jacob Vernau <jacobvernau@gmail.com>
2022-07-12 16:33:02 -0600

    google calendar linked, but not populating

--- frontend/src/components/EventCalendar/EventCalendar.jsx
+++ frontend/src/components/EventCalendar/EventCalendar.jsx
@@ -7,19 +7,30 @@ import "./eventcalendar.css";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // allows click events
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
 
 function EventCalendar() {
+  const googleCalendarURL = process.env.REACT_APP_GOOGLE_API_KEY;
+  // const myGoogleCalendarID = process.env.REACT_APP_GOOGLE_ID;
+  const myGoogleCalendarID = "slickdaddybrewing@gmail.com";
+
+  const herokuURL = "https://back-brewery.herokuapp.com/events/";
+
   // When a date grid is clicked, this function will invoke
   const handleDateClick = (dateClickInfo) => {
     console.log(dateClickInfo.dateStr);
   };
 
+  console.log(myGoogleCalendarID);
+
   return (
     <div className="calendar-container ">
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, googleCalendarPlugin]}
         initialView="dayGridMonth"
         dateClick={handleDateClick}
+        googleCalendarApiKey={googleCalendarURL}
+        events={[{ googleCalendarId: myGoogleCalendarID }]}
       />
     </div>
   );
--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+// Application components
+import TopNav from "../../components/TopNav/TopNav";
 import EventCalendar from "../../components/EventCalendar/EventCalendar";
+import Footer from "../../components/Footer/Footer";
 
 function Events() {
   return (
-    <div className="bg-dark text-white">
+    <div className="">
+      {/* <TopNav /> */}
       <h1>Events</h1>
       <EventCalendar />
+      {/* <Footer /> */}
     </div>
   );
 }
