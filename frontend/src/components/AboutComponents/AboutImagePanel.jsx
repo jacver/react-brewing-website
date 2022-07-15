f50a5935a6a9c0abfa0a0c5a85e1e5a64b404035
Jacob Vernau <jacobvernau@gmail.com>
2022-07-15 14:21:53 -0600

    added flex properties to tripanel images

--- frontend/src/components/AboutComponents/AboutImagePanel.jsx
+++ frontend/src/components/AboutComponents/AboutImagePanel.jsx
@@ -4,7 +4,7 @@ import Image from "react-bootstrap/Image";
 
 function AboutImagePanel() {
   return (
-    <div className="image-panel-container bg-dark d-flex justify-content-evenly pb-5">
+    <div className="image-panel-container bg-dark d-flex flex-column flex-md-row justify-content-evenly pb-5">
       <Image src="https://images.pexels.com/photos/5858064/pexels-photo-5858064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       <Image src="https://images.pexels.com/photos/5532715/pexels-photo-5532715.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       <Image src="https://images.pexels.com/photos/11722869/pexels-photo-11722869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
