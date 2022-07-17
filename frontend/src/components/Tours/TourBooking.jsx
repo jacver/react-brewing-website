8400d11e8df92ca050b339f2e85ce13cfcc0b337
Jacob Vernau <jacobvernau@gmail.com>
2022-07-17 12:11:01 -0600

    route and components established for tours

---
+++ frontend/src/components/Tours/TourBooking.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TourBooking() {
+  return <div>TourBooking</div>;
+}
+
+export default TourBooking;
---
+++ frontend/src/pages/Tours/Tours.jsx
@@ -0,0 +1,20 @@
+// src/pages/Tours/Tours.jsx
+
+import React from "react";
+
+// Component Imports
+import TopNav from "../../components/TopNav/TopNav";
+import TourBooking from "../../components/Tours/TourBooking";
+import Footer from "../../components/Footer/Footer";
+
+function Tours() {
+  return (
+    <div>
+      <TopNav />
+      <TourBooking />
+      <Footer />
+    </div>
+  );
+}
+
+export default Tours;
