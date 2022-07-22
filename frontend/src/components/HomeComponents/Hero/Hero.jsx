dea92ae57abcbaceff8571eaec2796bd3a8bfe86
Jacob Vernau <jacobvernau@gmail.com>
2022-07-22 16:42:41 -0600

    cards for navigating site

--- frontend/src/components/HomeComponents/Hero/Hero.jsx
+++ frontend/src/components/HomeComponents/Hero/Hero.jsx
@@ -11,14 +11,16 @@ function Hero() {
     <div className="hero d-flex flex-column justify-content-evenly">
       <section>
         <div className="hero-inner ">
-          <h1>SLICK DADDY'S</h1>
-          <h2>BREWING COMPANY</h2>
+          <h1>
+            SLICK DADDY'S <br />
+            BREWING COMPANY
+          </h1>
         </div>
       </section>
       <a
         className="hero-scroll"
         href="#homeAbout"
-        style={{ fontSize: "5rem", textDecoration: "none", color: "#59ff48" }}
+        style={{ fontSize: "10rem", textDecoration: "none", color: "#59ff48" }}
       >
         <HiChevronDoubleDown />
       </a>
--- frontend/src/components/HomeComponents/HomeBanner/HomeBanner.jsx
+++ frontend/src/components/HomeComponents/HomeBanner/HomeBanner.jsx
@@ -6,7 +6,7 @@ function HomeBanner() {
       className="homeBannerContainer d-flex flex-column justify-content-center align-items-center p-5 bg-dark text-white fs-5"
       id="homeAbout"
     >
-      <h2 className="vintage-font" style={{ color: "#59ff48" }}>
+      <h2 className="vintage-font fs-1" style={{ color: "#59ff48" }}>
         ABOUT US
       </h2>
       <p className="w-80">
