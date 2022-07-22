890339672354a22cd20cdb56bbfd2dabf8a0ff6e
Jacob Vernau <jacobvernau@gmail.com>
2022-07-21 18:37:29 -0600

    added alert box to let users know about heroku delay

---
+++ frontend/src/components/AlertBox/AlertBox.jsx
@@ -0,0 +1,23 @@
+// src/components/AlertBox/AlertBox.jsx
+
+import React, { useState } from "react";
+
+import Alert from "react-bootstrap/Alert";
+
+function AlertBox() {
+  const [show, setShow] = useState(true);
+  if (show) {
+    return (
+      <Alert variant="dark" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Looking for events?</Alert.Heading>
+        <p>
+          This component fetches data from a database hosted on Heroku. Heroku
+          apps can take ~10 seconds to "wake-up" if they haven't been used for a
+          few hours. If you're not seeing any data this might be why!
+        </p>
+      </Alert>
+    );
+  }
+}
+
+export default AlertBox;
--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // Application components
 import TopNav from "../../components/TopNav/TopNav";
+import AlertBox from "../../components/AlertBox/AlertBox";
 import EventCalendar from "../../components/EventCalendar/EventCalendar";
 import Footer from "../../components/Footer/Footer";
 
@@ -9,6 +10,7 @@ function Events() {
   return (
     <div className="bg-dark">
       <TopNav />
+      <AlertBox />
       <h1>Events</h1>
       <EventCalendar />
       <Footer />
